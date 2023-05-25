a2b2c17ce848fdd317f4e6cec201d56bbe0a1b0a
Николай Марусов <n.marusov@atol.ru>
2023-05-25 23:51:24 +0300

    Добавлен тест на единственность вакансии по id

--- src/tests/test_jobs.py
+++ src/tests/test_jobs.py
@@ -34,6 +34,25 @@ async def test_get_by_id(sa_session):
 
 
 @pytest.mark.asyncio
+async def test_get_by_id_single(sa_session):
+    user = UserFactory.build()
+    sa_session.add(user)
+    job_1 = JobFactory.build(user_id=user.id)
+    sa_session.add(job_1)
+    job_2 = JobFactory.build(user_id=user.id)
+    sa_session.add(job_2)
+    await sa_session.flush()
+
+    got_job = await job_query.get_by_id(sa_session, job_1.id)
+    assert got_job is not None
+    assert got_job.id == job_1.id
+
+    got_job = await job_query.get_by_id(sa_session, job_2.id)
+    assert got_job is not None
+    assert got_job.id == job_2.id
+
+
+@pytest.mark.asyncio
 async def test_create(sa_session):
     user = UserFactory.build(is_company=True)
     sa_session.add(user)
