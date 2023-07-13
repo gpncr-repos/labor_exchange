f38b9c1b30a23fec624bd30533bc04ac7ca841a7
elnars <elnar.sadreev@gmail.com>
2023-07-13 12:47:03 +0300

    Add columns to jobs and responses

--- src/models/responses.py
+++ src/models/responses.py
@@ -10,7 +10,7 @@ class Response(Base):
     user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), comment="Идентификатор пользователя")
     job_id = sa.Column(sa.Integer, sa.ForeignKey('jobs.id'), comment="Идентификатор вакансии")
 
-    # добавьте ваши колонки сюда
+    message = sa.Column(sa.String, comment="Сопроводительное письмо")
 
     user = relationship("User", back_populates="responses")
     job = relationship("Job", back_populates="responses")
