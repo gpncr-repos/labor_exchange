98be5944c8c5376e47a8922e017fd23d29112f05
Nik <nikstarall@gmail.com>
2023-10-23 16:00:30 +0700

    feat: expend Job and Responses tables by new columns

--- src/models/jobs.py
+++ src/models/jobs.py
@@ -1,3 +1,5 @@
+import datetime
+
 from db_settings import Base
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship
@@ -8,8 +10,12 @@ class Job(Base):
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, comment="Идентификатор вакансии")
     user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), comment="Идентификатор пользователя")
-
-    # добавьте ваши колонки сюда
+    title = sa.Column(sa.String, comment="Название вакансии")
+    description = sa.Column(sa.String, comment="Описание ввкансии")
+    salary_from = sa.Column(sa.Integer, comment="Зарплата от")
+    salary_to = sa.Column(sa.Integer, comment="Зарплата до")
+    is_active = sa.Column(sa.Boolean, default=False)
+    created_at = sa.Column(sa.DateTime, default=datetime.datetime.utcnow)
 
     user = relationship("User", back_populates="jobs")
     responses = relationship("Response", back_populates="job")
