64d44e861c8f9772400ecd65c8d0e541a6937f49
SedynovIvan <SedynovIS@gmail.com>
2024-07-21 15:00:00 +0300

    Изменил таблицу Responses- добавил колонку massage

--- src/models/responses.py
+++ src/models/responses.py
@@ -9,8 +9,7 @@ class Response(Base):
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, comment="Идентификатор отклика")
     user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), comment="Идентификатор пользователя")
     job_id = sa.Column(sa.Integer, sa.ForeignKey('jobs.id'), comment="Идентификатор вакансии")
-
-    # добавьте ваши колонки сюда
+    massage=sa.Column(sa.String,comment="Сопроводительное письмо")
 
     user = relationship("User", back_populates="responses")
     job = relationship("Job", back_populates="responses")
