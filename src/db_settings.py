84401be7afa81126844c2e0e1f8f83ccefd94ccd
Иван <SedynovIS@gmail.com>
2024-08-04 21:16:24 +0300

    add test bd

--- src/db_settings.py
+++ src/db_settings.py
@@ -8,9 +8,9 @@ DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 DB_HOST = os.environ.get('DB_HOST')
 DB_NAME = os.environ.get('DB_NAME')
-
+DB_PORT = os.environ.get('DB_PORT')
 SQLALCHEMY_DATABASE_URL = (
-    f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+    f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 )
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
